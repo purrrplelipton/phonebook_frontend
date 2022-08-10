1c73976fb98363dd2e0251cdf1fd6e060077bee0
purrrplelipton <immanueltoby@gmail.com>
2022-08-10 15:32:11 +0100

    first commit: rollback to working err functions

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ const App = () => {
   const [persons, setPersons] = useState([]);
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [birthdate, setBirthdate] = useState("");
-  const [gender, setGender] = useState("non-binary");
   const [filterQuery, setFilterQuery] = useState("");
   const [errorMessage, setErrorMessage] = useState({
     success: null,
@@ -87,12 +83,6 @@ const App = () => {
       const personObject = {
         name: name,
         number: number,
-        email: email,
-        address: address,
-        birthdate: birthdate,
-        gender: gender,
-        id: persons.length + 1,
-        date: new Date(),
       };
       personsService
         .create(personObject)
@@ -104,10 +94,6 @@ const App = () => {
           });
           setName("");
           setNumber("");
-          setEmail("");
-          setAddress("");
-          setBirthdate("");
-          setGender("non-binary");
           setTimeout(
             () => setErrorMessage({ success: null, message: null }),
             3000
@@ -177,16 +163,15 @@ const App = () => {
 
   const formNameChange = (event) => setName(event.target.value);
   const formNumChange = (event) => setNumber(event.target.value);
-  const formEmailChange = (event) => setEmail(event.target.value);
-  const formAddressChange = (event) => setAddress(event.target.value);
-  const formBirthdateChange = (event) => setBirthdate(event.target.value);
-  const formGenderChange = (value) => setGender(value);
 
   return (
     <Aux>
       <h1>Phonebook</h1>
       <Notification message={errorMessage} />
-      <input placeholder="filter by..." onChange={changeFilterQuery} />
+      <label>
+        Filter By
+        <input onChange={changeFilterQuery} value={filterQuery} />
+      </label>
       <h1>add new contact</h1>
       <Form
         submit={submitHandler}
@@ -194,14 +179,6 @@ const App = () => {
         nameChange={formNameChange}
         number={number}
         numChange={formNumChange}
-        email={email}
-        emailChange={formEmailChange}
-        address={address}
-        addressChange={formAddressChange}
-        birthdate={birthdate}
-        birthdateChange={formBirthdateChange}
-        gender={gender}
-        genderChange={formGenderChange}
       />
       <h1>numbers</h1>
       <div className="phonebook-entries">
